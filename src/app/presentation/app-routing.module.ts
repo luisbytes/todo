95a6221d48255ae90061794c1fc9bfa522697eb0
luisbytes <luisbytesdev@gmail.com>
2026-05-03 08:39:35 -0500

    feat: implement Add Todo page with routing and UI components

--- src/app/presentation/app-routing.module.ts
+++ src/app/presentation/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'add-todo',
+    loadChildren: () => import('./pages/add-todo/add-todo.module').then( m => m.AddTodoPageModule)
+  },
 ];
 
 @NgModule({
---
+++ src/app/presentation/pages/add-todo/add-todo.page.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-todo',
+  templateUrl: './add-todo.page.html',
+  styleUrls: ['./add-todo.page.scss'],
+  standalone: false,
+})
+export class AddTodoPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
