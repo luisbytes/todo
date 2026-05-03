b3c99b5590a026f9f0c02b5d2fe72fc5e032d03b
luisbytes <luisbytesdev@gmail.com>
2026-05-03 08:25:03 -0500

    refactor: move AppComponent import to app.module.ts and remove unused presentation module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './presentation/app-routing.module';
+import { AppComponent } from './presentation/app.component';
 
 @NgModule({
   declarations: [AppComponent],
